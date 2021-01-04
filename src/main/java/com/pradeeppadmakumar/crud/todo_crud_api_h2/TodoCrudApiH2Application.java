fd7a46a332991a8403fa803b1a8bc198c3ba259c
Pradeep Padmakumar <pradeep.padmakumar91@gmail.com>
2021-01-03 21:02:40 -0600

    initial code

---
+++ src/main/java/com/pradeeppadmakumar/crud/todo_crud_api_h2/TodoCrudApiH2Application.java
@@ -0,0 +1,13 @@
+package com.pradeeppadmakumar.crud.todo_crud_api_h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoCrudApiH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoCrudApiH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pradeeppadmakumar/crud/todo_crud_api_h2/TodoCrudApiH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradeeppadmakumar.crud.todo_crud_api_h2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoCrudApiH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
